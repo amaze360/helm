e6f31bc8eb6cab18c8378d58cd18a91c65df61e1
FreekO <info@f3k.tech>
2023-12-06 09:39:03 +0300

    add process chart script

---
+++ scripts/process-charts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Loop through all chart directories
+for d in */ ; do
+  chart_name=$(basename "$d")
+  chart_version=$(yq e '.version' "$d/Chart.yaml")
+
+  image_tag=$(yq e '.image.tag' "$d/values.yaml") yq -i '.appVersion=env(image_tag)' "$d/Chart.yaml"   
+
+  if [ ! -f "../$chart_name-$chart_version.tgz" ]; then
+    helm package "$d" -d ..
+  else
+    echo "Chart $chart_name-$chart_version already exists, skipping."
+  fi
+done
