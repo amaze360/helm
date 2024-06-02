a791ccf9e9e28fb773527e134a48f3f1f2a561db
FreekO <info@f3k.tech>
2024-06-02 12:11:56 +0300

    add depency update

--- scripts/process-charts.sh
+++ scripts/process-charts.sh
@@ -5,7 +5,8 @@ for d in */ ; do
   chart_name=$(basename "$d")
   chart_version=$(grep 'version:' "$d/Chart.yaml" | awk '{ print $2 }')
 
-  if [ ! -f "../public/$chart_name-$chart_version.tgz" ]; then
+  if [ ! -f "../public/$chart_name-$chart_version.tgz" ]; thenhelm dependency update ./charts/api360
+    helm dependency update "$d"
     helm package "$d" -d ../public
   else
     echo "Chart $chart_name-$chart_version already exists, skipping."
