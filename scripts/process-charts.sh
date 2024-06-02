5da3e63c12691ac193858cf863ae72367df18184
FreekO <info@f3k.tech>
2024-06-02 12:22:05 +0300

    remove accidentally pasted txt

--- scripts/process-charts.sh
+++ scripts/process-charts.sh
@@ -5,7 +5,7 @@ for d in */ ; do
   chart_name=$(basename "$d")
   chart_version=$(grep 'version:' "$d/Chart.yaml" | awk '{ print $2 }')
 
-  if [ ! -f "../public/$chart_name-$chart_version.tgz" ]; thenhelm dependency update ./charts/api360
+  if [ ! -f "../public/$chart_name-$chart_version.tgz" ]; then
     helm dependency update "$d"
     helm package "$d" -d ../public
   else
